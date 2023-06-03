781f1eb03baa60b64fe25df8caad47c8cd98d93e
EC2 Default User <ec2-user@ip-172-31-17-58.ap-southeast-1.compute.internal>
2023-06-03 17:22:41 +0000

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6e11994364e58261116fa342211f5df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9e56932f49d0d68dede2a1f6873ed8",
+    "url": "/celo-charity-campaign/index.html"
+  },
+  {
+    "revision": "14318f22541684429cda",
+    "url": "/celo-charity-campaign/static/css/main.5f689838.chunk.css"
+  },
+  {
+    "revision": "8502a01f3e36dba84da1",
+    "url": "/celo-charity-campaign/static/js/2.21c536a9.chunk.js"
+  },
+  {
+    "revision": "14318f22541684429cda",
+    "url": "/celo-charity-campaign/static/js/main.7b97398a.chunk.js"
+  },
+  {
+    "revision": "742a20075989541f0a97",
+    "url": "/celo-charity-campaign/static/js/runtime-main.0260cd2c.js"
+  }
+]);
